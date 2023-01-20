d2c6f5d6a9821e2c124004c2a0b67df806fc5d3c
cdsalty <soltiscd@gmail.com>
2023-01-19 21:19:57 -0500

    updated lesson from jan19th on partias and MIDDLEWARE

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+console.log('Time is now: ', Date.now());
+console.log("This is the first line of defense. It's a middleware function.");
+
 const http = require('http');
 const express = require('express');
 const templateEngine = require('express-es6-template-engine');
@@ -5,7 +8,6 @@ const app = express();
 app.engine('html', templateEngine);
 app.set('views', 'templates');
 app.set('view engine', 'html');
-
 const cats = [
   {
     id: 'abys',
@@ -17,10 +19,22 @@ const cats = [
   },
 ];
 
+app.use((req, res, next) => {
+  console.log('Time is now: ', Date.now());
+  console.log("This is the first line of defense. It's a middleware function.");
+  next();
+});
+
+app.use('/cats', (req, res, next) => {
+  console.log('Request type is ', req.method);
+  next();
+});
+
 app.get('/', (req, res) => {
   res.render('homepage', {
     partials: {
       header: 'partials/header',
+      footer: 'partials/footer',
     },
   });
 });
@@ -38,6 +52,7 @@ app.get('/cats', (req, res) => {
 
 app.get('/cats/:catId', (req, res) => {
   const catId = req.params['catId'];
+  console.log('The catId is: ', catId);
   const cat = cats.find((c) => c.id == catId);
   res.render('catinfo', {
     locals: {
